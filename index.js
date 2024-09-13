2dc1d6df275d7fae471d37525321f1fe7546c273
EduardoRibero <ribeirodomingues2020@gmail.com>
2024-09-13 03:22:14 -0300

    config cors

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const express = require('express')
-const app = express()
-
-//Arquivo de rotas
 const router = require('./routers/index')
-router(app, express)
-
+const express = require('express')
+const cors = require('cors');
 //Arquivos de acesso a dados
 const conexao = require('./infra/conexao')
 const TabelaTarefas = require('./infra/Tarefas')
+
+const app = express()
+
+app.use(cors());
+
+//Arquivo de rotas
+router(app, express)
 
 //Conexão com Banco de Dados
 TabelaTarefas.init(conexao)
